74e344a15eec51c93d4ea067ff3e8282f9e25125
SINeWang <im.sinewang@gmail.com>
2017-06-02 21:30:55 +0800

    refactor

---
+++ src/app/instances/instances.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {Headers, Http, RequestOptions, Response} from '@angular/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs/Observable';
+import {Status} from './status.data';
+import {Subscriptions} from '../subscriptions/subscriptions.data';
+
+@Injectable()
+export class InstancesService {
+
+  private URL = environment.kiimate_url;
+
+  constructor(private http: Http) {
+  }
+
+  visit(subscriptions: Subscriptions): Observable<Status> {
+    const headers = new Headers({
+      // 'Authorization': authorization,
+      'X-SUMMER-VisitorId': 'wangyj',
+      'X-SUMMER-RequestId': Math.random()
+    });
+    const options = new RequestOptions({headers: headers});
+
+    const url = this.URL + '/' + subscriptions.subscriberId + '/status/' + subscriptions.id;
+    return this.http.get(url, options)
+      .map((res: Response) => res.json() || [])
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  commit(status: Status): Observable<Status> {
+    const headers = new Headers({
+      // 'Authorization': authorization,
+      'X-SUMMER-OperatorId': 'wangyj',
+      'X-SUMMER-RequestId': Math.random()
+    });
+    const options = new RequestOptions({headers: headers});
+
+    const url = this.URL + '/' + status.ownerId + '/status/' + status.subId;
+    return this.http.put(url, status.map, options)
+      .map((res: Response) => res.json() || [])
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+}
